419499b2487b9d04503d61a4a59173e335d5c134
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-10-26 04:28:09 +0900

    init

--- getStart/src/app.ts
+++ getStart/src/app.ts
@@ -3,18 +3,25 @@ import { Cat, CatType } from "./app.model";
 const app: express.Express = express();
 const port: number = 8000;
 
-app.get("/", (req: express.Request, res: express.Response) => {
+app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
+  next();
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
 });
 
 app.get("/cat/blue", (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send({ blue: Cat[0] });
 });
 app.get("/cat/some", (req: express.Request, res: express.Response) => {
-  console.log(req.rawHeaders[1]);
   res.send({ blue: Cat[1] });
+});
+
+app.use((req, res, next) => {
+  console.log("404 not found middleware");
+  res.send({ error: "404 not found" });
 });
 
 app.listen(port, () => {
