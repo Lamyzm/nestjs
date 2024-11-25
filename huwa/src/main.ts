58fdcfc624d0bb5b0ae705971a5ee4470a40bfb9
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-11-25 22:17:11 +0900

    validation pipe 추가

--- huwa/src/main.ts
+++ huwa/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from 'src/http-exception.filter';
+
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //  *등록! classValidation 사용
   app.useGlobalFilters(new HttpExceptionFilter()); // 글로벌로 Filter 적용
   await app.listen(process.env.PORT ?? 8000);
 }
