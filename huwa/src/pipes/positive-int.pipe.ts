8662b0eec6f223030d4b8b1ce7d9627816052255
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-11-25 22:16:37 +0900

    커스텀 pipe

--- huwa/src/pipes/positive-int.pipe.ts
+++ huwa/src/pipes/positive-int.pipe.ts
@@ -2,5 +2,11 @@ import { HttpException, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class PositiveIntPipe implements PipeTransform {
-  transform() {}
+  transform(value: number) {
+    if (value < 0) {
+      console.log(Math.abs(value), value);
+      throw new HttpException('value > 0', 400);
+    }
+    return value;
+  }
 }
